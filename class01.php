98fe77375d2c0738a61e29a40c6f0b7ae22948c7
thales.rollo <thalestgr@hotmail.com>
2022-09-14 20:00:54 -0300

    Criando Class01

---
+++ class01.php
@@ -0,0 +1,42 @@
+
+<?php 
+// Set User Name
+git config --global user.name "thales.rollo"
+
+// Set E-mail
+git config --global user.email "thalestgr@hotmail.com"
+
+// Get User Name
+git config user.name
+
+// Get E-mail
+git config user.email
+
+// Create a repository
+// Inside the folder
+git init 
+
+// Check status
+git status
+
+// Add file
+git add filename
+
+// Commit a single file
+git commit filename -m "Message"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
